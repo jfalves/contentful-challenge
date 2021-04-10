b3a9658e63286fac4cfd4be75f620a5ad39e2e09
Jonathan <jonathan.f.alves@gmail.com>
2021-04-10 23:27:12 +0200

    Initial setup of postgres as a service of docker

---
+++ config/docker-entrypoint-initdb.d/init-postgres_database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE datawarehouse;
+
+CREATE SCHEMA
+IF NOT EXISTS staging;
+CREATE SCHEMA
+IF NOT EXISTS historical;
+CREATE SCHEMA
+IF NOT EXISTS dimensional;
+
+GRANT ALL PRIVILEGES ON DATABASE datawarehouse TO postgres;
