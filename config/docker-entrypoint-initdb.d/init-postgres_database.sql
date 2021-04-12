763514c08a6c4ebd924c024a4926b8b673e6ff71
Jonathan <jonathan.f.alves@gmail.com>
2021-04-12 14:27:55 +0200

    Include one more schema (snapshots) to be created on postgres init database

--- config/docker-entrypoint-initdb.d/init-postgres_database.sql
+++ config/docker-entrypoint-initdb.d/init-postgres_database.sql
@@ -5,6 +5,8 @@ IF NOT EXISTS staging;
 CREATE SCHEMA
 IF NOT EXISTS historical;
 CREATE SCHEMA
+IF NOT EXISTS snapshots;
+CREATE SCHEMA
 IF NOT EXISTS dimensional;
 
 GRANT ALL PRIVILEGES ON DATABASE datawarehouse TO postgres;
