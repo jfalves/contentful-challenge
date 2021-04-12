768aef06d8a9faa2e19f9853db30fa7eb109ef25
Jonathan <jonathan.f.alves@gmail.com>
2021-04-12 21:43:34 +0200

    Add staging table structures to initial setup

--- config/docker-entrypoint-initdb.d/init-postgres_database.sql
+++ config/docker-entrypoint-initdb.d/init-postgres_database.sql
@@ -9,4 +9,16 @@ IF NOT EXISTS snapshots;
 CREATE SCHEMA
 IF NOT EXISTS dimensional;
 
+CREATE TABLE staging."event"
+(
+    "date" date NULL,
+    json_content json NULL
+);
+
+CREATE TABLE staging.organization
+(
+    "date" date NULL,
+    json_content json NULL
+);
+
 GRANT ALL PRIVILEGES ON DATABASE datawarehouse TO postgres;
